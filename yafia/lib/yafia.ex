29c6c8da83e86b8ed7339822fcfb9c4fb81912f7
Jean Amourrouge <jedi-papstjl@gmx.de>
2024-02-27 22:19:30 +0100

    init
    Initial Commit

---
+++ yafia/lib/yafia.ex
@@ -0,0 +1,18 @@
+defmodule Yafia do
+  @moduledoc """
+  Documentation for `Yafia`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Yafia.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ yafia/test/yafia_test.exs
@@ -0,0 +1,8 @@
+defmodule YafiaTest do
+  use ExUnit.Case
+  doctest Yafia
+
+  test "greets the world" do
+    assert Yafia.hello() == :world
+  end
+end
