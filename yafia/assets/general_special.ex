7cd9ca3458cfedb8900f00de22dc3bfc8db1623c
Jean Amourrouge <jedi-papstjl@gmx.de>
2024-03-09 03:19:28 +0100

    general special

---
+++ yafia/assets/general_special.ex
@@ -0,0 +1,8 @@
+defmodule General_Special do
+  _images = %{
+              'd'=> {0,0,78,78},
+              'hs'=> {79,0,78,78},
+              's'=> {157,0,78,78}
+  }
+  def info(), do: ["assets/general_special.png": _images] 
+end
