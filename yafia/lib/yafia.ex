43f93cd44a12b8ec2f5543a0211b28dc90b7bf9a
Jean Amourrouge <jedi-papstjl@gmx.de>
2024-03-10 02:38:01 +0100

    find with do
    double generator

--- yafia/lib/yafia.ex
+++ yafia/lib/yafia.ex
@@ -31,7 +31,28 @@ defmodule Yafia do
     StreetFighter.inputs()
   end
 
+  def test do
+    inp = "2 mp , 4 mp > hp qcb mk srk 2p"
+
+    Parser.split_ws(inp)
+    |> find(StreetFighter.inputs())
+  end
+
   def sys do
     MoveImage.sys_crop("assets/standard_directionals.png", 0, 0, 55, 56, 00)
+  end
+
+  defp dim({:ok, result}) do
+    {x0, y0, xw, yh} = result
+    # MoveImage.crop_input(x0, y0, xw, yh)
+  end
+
+  defp dim(:error), do: :error
+
+  defp find(moves, game) do
+    for {img, parts} <- game,
+        move <- moves,
+        into: %{},
+        do: dim(Map.fetch(parts, move))
   end
 end
